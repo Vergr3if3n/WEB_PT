b3586fec88b6e91dd7d95109f3ba3fe0969147d4
Nil <Nil>
2024-04-25 20:03:17 +0300

    two

--- js/button_script.js
+++ js/button_script.js
@@ -3,7 +3,7 @@ var count = 0;
 document.getElementById("button1").onclick = function () {
 	if (count == 0) { 
 		var img = document.createElement("img");
-		img.src = "/home/nil/pt/WEB_PT/image/promocode.png"
+		img.src = "../image/promocode.png"
 		document.getElementById("demo").appendChild(img) 
 	} else {
 		document.getElemetnById("demo").innerHTML = "";
